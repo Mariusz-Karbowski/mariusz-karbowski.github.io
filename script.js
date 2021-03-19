12cf2e9af3f85d5f2d8e4e5284edf1c76064026e
Mariusz-Karbowski <m.karbowski@outlook.com>
2021-03-19 17:08:51 +0100

    CRUD fix and update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'http://https://vast-plateau-90382.herokuapp.com/v1/task/';
+   const apiRoot = 'https://vast-plateau-90382.herokuapp.com/v1/task/';
    const trelloApiRoot = 'http://localhost:8080/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
